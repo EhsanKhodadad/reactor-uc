0d8ed763728cc14875fded06963a8bd989c17adb
Ehsan Khodadad <ehkh@dtu.dk>
2026-03-02 14:52:14 +0100

    force self-hosted to use fpga

--- examples/patmos/buildAll.sh
+++ examples/patmos/buildAll.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+while getopts ":s" opt; do 
+  case $opt in
+    s) SLEF_HOSTED=true;;
+    :) echo "Option -$OPTARG requires an argument." >&2; exit 1;;
+    \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
+  esac
+done
+
 if ! command -v pasim &> /dev/null; then
   echo "Error: pasim command not found. Please ensure it is installed and available in your PATH."
 else
@@ -19,7 +27,13 @@ else
             echo "Entering $dir"
             pushd "$dir"
             chmod +x build.sh
-            ./build.sh
+            if [ "$SLEF_HOSTED" = true ]; then
+              echo "Running build.sh for self-hosted runner"
+              ./build.sh -f
+            else
+              echo "Running build.sh for non-self-hosted runner"
+              ./build.sh -e
+            fi
             popd
         fi
     done
